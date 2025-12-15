ddd63b80bbe8ea94636f4f687055526219bb6673
Julian-Limbu <julianlimbu514@gmail.com>
2025-12-15 00:09:44 +0000

    Player Class
    Introduced PlayerClass.

---
+++ Assets/PlayerClass.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerClass : MonoBehaviour
+{
+    public static PlayerClass instance; // This is the player.
+
+    void Awake()
+    {
+        // Check if there is already a player.
+        if (instance != null)
+        {
+            // if not not this one then delete this
+            if (instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        else
+        {
+            // Set this as the player
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
