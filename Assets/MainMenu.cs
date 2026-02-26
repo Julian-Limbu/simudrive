17e0def6f1bcd5871d741355ac7cda4629a05d7e
Julian-Limbu <julianlimbu514@gmail.com>
2026-02-26 12:54:52 +0000

    Update MainMenu.cs
    scenarios commented code

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -4,7 +4,42 @@ using UnityEngine.SceneManagement; // The C# namespace in Unity used for control
 public class MainMenu : MonoBehaviour
 {
     /*
-    Scenarios()
+    public void Scenarios() 
+    {
+        string[] scenarioOptions = {"Learning", "Racing", "Free Roam"}; // Selections align with these elements - contribute to the overall option; applies to the array 'vehicle' too.
+        string[] vehicleOptions = {"Car", "Motorcycle"};
+
+        string[] options = {"",""};
+        int scenarioSelect = 0; // Caused by the action of pressing a button; applies to the variable 'vehicleSelect' too.
+        int vehicleSelect = 0;
+
+        // Scenarios:
+        if (scenarioSelect == 1)
+        {
+            options[0] = "Learning";
+        }
+        else if (scenarioSelect == 2)
+        {
+            options[0] = "Racing";
+        }
+        else
+        {
+            options[0] = "Free Roam";
+        }
+
+        // Vehicles:
+        if (vehicleSelect == 1)
+        {
+            options[1] = "Car";
+        }
+        else 
+        {
+            options[2] = "Motorcycle";
+        }
+        
+    }
+        
+    }
 
     Settings()
 
